cfd5b9c7bb430d3611fcb522fa7cb0ade81e68d8
JoePinoOG <estay980@gmail.com>
2024-06-11 23:50:05 -0400

    solucionando problema vistas e imagenes

--- teboxeo/urls.py
+++ teboxeo/urls.py
@@ -2,14 +2,10 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = {
-path('',views.index, name = 'inicio'),
-path('productos/',views.productos, name = 'productos'),
-path('servicios/',views.servicios, name = 'servicios'),
-path('contacto/',views.contacto, name = 'contacto'),
-path('login/',views.login, name = 'login')
-
-
-
-
-}
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('productos/',views.productos, name = 'productos'),
+    path('servicios/',views.servicios, name = 'servicios'),
+    path('contacto/',views.contacto, name = 'contacto'),
+    path('login/',views.login, name = 'login')
+]
